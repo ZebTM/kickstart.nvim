af1af65ce2866a539eedf7638320616c3ed89fce
Zachary Brown <brownz4@mail.uc.edu>
2024-11-18 12:22:42 -0500

    home-manager setup

---
+++ plugin/other.lua
@@ -0,0 +1,11 @@
+-- Lualine
+require("lualine").setup({
+    icons_enabled = true,
+    theme = 'onedark',
+})
+
+-- Colorscheme
+-- vim.cmd("colorscheme gruvbox")
+vim.cmd("colorsheme tokyonight-night")
+-- Comment
+require("Comment").setup()
